d4703287dac9f0f4109d4e4ec4163d2ae67a3a0b
randomriffs <pratheesheswaran@gmail.com>
2022-10-15 12:56:26 +0530

    added missing files

--- index.js
+++ index.js
@@ -8,74 +8,86 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3002;
 var router = express.Router();
-var router= require('./router/index');
+var router = require('./router/index');
 app.use('/', router);
 var cron = require('node-cron');
 require('dotenv').config();
+const { pinToTumblr } = require('./modules/pinToTumblr');
 
 // app.use(express.static("build"));
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/', function (req, res) {
     // res.send('Server is up')
-    res.sendFile(path.join(__dirname+'/build/index.html'));
-    
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+
 });
 
-if(true){
-// scriptEcho prod 0 * * * *
-var scriptEcho = require('./firestore/scriptEcho')
-// scriptEcho.tweet();
-cron.schedule("20 * * * * ", () => {
+if (true) {
+    // scriptEcho prod 0 * * * *
+    var scriptEcho = require('./firestore/scriptEcho')
     scriptEcho.tweet();
-});
+    // cron.schedule("20 * * * * ", () => {
+    //     scriptEcho.tweet();
+    // });
+    // adas
 
-// of Old Had prod
-var ofOldHat=require('./firestore/ofOldHat')
-// ofOldHat.tweet();
-cron.schedule("20 * * * * ", () => {
-    ofOldHat.tweet();
-});
+    // of Old Had prod
+    var ofOldHat = require('./firestore/ofOldHat')
+    // ofOldHat.tweet();
+    cron.schedule("20 * * * * ", () => {
+        ofOldHat.tweet();
+    });
 
-// gooseWritings prod
-var gooseWritings = require('./modules/gooseWritings');
-// gooseWritings.goose();
-cron.schedule("0 */6 * * *", () => {
-gooseWritings.goose();
-});
+    // gooseWritings prod
+    var gooseWritings = require('./modules/gooseWritings');
+    // gooseWritings.goose();
+    cron.schedule("0 */6 * * *", () => {
+        gooseWritings.goose();
+    });
 
-// fuckWithWord prod
-var fuckWithWord = require('./firestore/fuckWithWord')
-// fuckWithWord.tweet();
-cron.schedule("29 * * * *", () => {
-fuckWithWord.tweet();
-});
+    // fuckWithWord prod
+    var fuckWithWord = require('./firestore/fuckWithWord')
+    // fuckWithWord.tweet();
+    cron.schedule("29 * * * *", () => {
+        fuckWithWord.tweet();
+    });
 
-// typewriter news prod
-var typewriterNews = require('./modules/typewriterNews')
-// typewriterNews.tweet();
-cron.schedule("*/15 * * * *", () => {
-typewriterNews.tweet();
-});
+    // typewriter news prod
+    var typewriterNews = require('./modules/typewriterNews')
+    // typewriterNews.tweet();
+    cron.schedule("*/15 * * * *", () => {
+        typewriterNews.tweet();
+    });
 
-// hey kitty kitty prod
-var heyKittyKitty = require('./firestore/heyKittyKitty');
-// heyKittyKitty.tweet()
-cron.schedule("0 * * * *", () => {
-    heyKittyKitty.tweet()
-});
+    // hey kitty kitty prod
+    var heyKittyKitty = require('./firestore/heyKittyKitty');
+    // heyKittyKitty.tweet()
+    cron.schedule("0 * * * *", () => {
+        heyKittyKitty.tweet()
+    });
 
-// sametypewriter prod 
-var sameTypewriter = require('./modules/sameTypewriter');
-// sameTypewriter.tweet();
-cron.schedule("0 */12 * * *", () => {
-    sameTypewriter.tweet();
-});
+    // sametypewriter prod 
+    var sameTypewriter = require('./modules/sameTypewriter');
+    // sameTypewriter.tweet();
+    cron.schedule("0 */12 * * *", () => {
+        sameTypewriter.tweet();
+    });
 
-// // usSlashUi prod
-// var uxSlashUi = require('./modules/uxSlashUi');
-// cron.schedule("*/15 * * * *", () => {
-//     uxSlashUi.tweet();
-// });
+    cron.schedule("*/10 * * * * *", () => {
+        pinToTumblr();
+    });
+
+
+    // // usSlashUi prod
+    // var uxSlashUi = require('./modules/uxSlashUi');
+    // cron.schedule("*/15 * * * *", () => {
+    //     uxSlashUi.tweet();
+    // });
+
+    // cron.schedule("20 * * * * ", () => {
+
+    // });
+
 }
 
 
@@ -83,7 +95,7 @@ cron.schedule("0 */12 * * *", () => {
 // var scrapDribb = require('./modules/dribb');
 // scrapDribb.scrapDrib()
 // cron.schedule('0 */24 * * *', () => {
-   
+
 // });
 
 // // sadLog.txt 
